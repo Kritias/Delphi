23d7dce69ea921b3b6df3054354668610eccd497
Maxim <antifrist@gmail.com>
2016-01-15 10:41:10 +0300

    Project7

---
+++ Project7/Unit1.pas
@@ -0,0 +1,113 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ScktComp;
+
+type
+  TForm1 = class(TForm)
+    ClientSocket1: TClientSocket;
+    ServerSocket1: TServerSocket;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Edit3: TEdit;
+    ListBox1: TListBox;
+    Label3: TLabel;
+    Edit4: TEdit;
+    Label4: TLabel;
+
+    procedure ServerSocket1ClientRead(Sender: TObject; Socket: TCustomWinSocket);
+    procedure ClientSocket1Read(Sender:TObject; Socket:TCustomWinSocket);
+    procedure Button1Click(Sender: TObject);
+    procedure ClientSocket1Connect(Sender: TObject;Socket: TCustomWinSocket);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure CR(Sender: TObject; Socket: TCustomWinSocket);
+    
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+if ClientSocket1.Active then begin
+ClientSocket1.Close;
+if ServerSocket1.Active then begin
+ServerSocket1.Close;
+Exit;
+end;
+end;
+try
+ClientSocket1.ClientType:=ctBlocking;
+ClientSocket1.Host:=Edit1.Text;
+ClientSocket1.Port:=StrToInt(Edit2.Text);
+ClientSocket1.Open;
+ServerSocket1.Port:=StrToInt(Edit2.Text);
+ServerSocket1.Open;
+ListBox1.Items.Add('Подключенно к ' + Edit1.Text+ ':' + Edit2.Text);
+except
+ListBox1.Items.Add('Адресс '+ Edit1.Text + ':'+ Edit2.Text + ' недоступен');
+end;
+end;
+
+procedure TForm1.ClientSocket1Connect(Sender: TObject;
+  Socket: TCustomWinSocket);
+begin
+
+Socket.SendText('Hello!');
+ListBox1.Items.Add('Вы: '+ '  Hello!');
+
+end;
+
+procedure TForm1.ClientSocket1Read(Sender:TObject; Socket:TCustomWinSocket);
+begin
+ListBox1.Items.Add('Собеседник: ' + Socket.ReceiveText);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+ClientSocket1.Socket.SendText(Edit3.Text);
+ListBox1.Items.Add('Вы: ' + Edit3.Text);
+end;
+
+procedure TForm1.ServerSocket1ClientRead(Sender: TObject; Socket: TCustomWinSocket);
+var s: string;
+begin
+ListBox1.Items.Add('Собеседник: ' + Socket.ReceiveText);
+end;
+
+
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+ServerSocket1:=TServerSocket.Create(Self);
+ServerSocket1.Port:=StrToInt(Edit4.Text);
+ServerSocket1.ServerType:=stNonBlocking;
+ServerSocket1.OnClientRead:=CR;
+ServerSocket1.Open();
+Application.ProcessMessages();
+ShowMessage('Сервер запущен ' + Edit4.Text);
+end;
+
+procedure TForm1.CR(Sender: TObject; Socket: TCustomWinSocket);
+begin
+ListBox1.Items.Add('sendServer' + Socket.ReceiveText());
+end;
+
+end.
---
+++ Project7/Unit1.~dfm
@@ -0,0 +1,127 @@
+object Form1: TForm1
+  Left = 188
+  Top = 227
+  Width = 602
+  Height = 340
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 128
+    Top = 16
+    Width = 10
+    Height = 13
+    Caption = 'IP'
+  end
+  object Label2: TLabel
+    Left = 248
+    Top = 16
+    Width = 41
+    Height = 21
+    Caption = 'Port'
+  end
+  object Label3: TLabel
+    Left = 320
+    Top = 16
+    Width = 58
+    Height = 21
+    Caption = #1057#1086#1086#1073#1097#1077#1085#1080#1103
+  end
+  object Label4: TLabel
+    Left = 192
+    Top = 232
+    Width = 91
+    Height = 13
+    Caption = #1055#1086#1088#1090' '#1076#1083#1103' '#1089#1077#1088#1074#1077#1088#1072
+  end
+  object Button1: TButton
+    Left = 8
+    Top = 40
+    Width = 105
+    Height = 25
+    Caption = #1055#1086#1076#1082#1072#1085#1077#1082#1090#1080#1090#1100#1089#1103
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 8
+    Top = 128
+    Width = 75
+    Height = 25
+    Caption = #1054#1090#1087#1088#1072#1074#1080#1090#1100
+    TabOrder = 1
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 16
+    Top = 232
+    Width = 89
+    Height = 25
+    Caption = #1057#1090#1072#1088#1090' '#1089#1077#1088#1074#1077#1088#1072' '
+    TabOrder = 2
+    OnClick = Button3Click
+  end
+  object Edit1: TEdit
+    Left = 128
+    Top = 40
+    Width = 105
+    Height = 21
+    TabOrder = 3
+    Text = 'localhost'
+  end
+  object Edit2: TEdit
+    Left = 248
+    Top = 40
+    Width = 57
+    Height = 21
+    TabOrder = 4
+    Text = '81'
+  end
+  object Edit3: TEdit
+    Left = 96
+    Top = 128
+    Width = 209
+    Height = 21
+    TabOrder = 5
+    Text = 'Hi boy'
+  end
+  object ListBox1: TListBox
+    Left = 320
+    Top = 40
+    Width = 249
+    Height = 249
+    ItemHeight = 13
+    TabOrder = 6
+  end
+  object Edit4: TEdit
+    Left = 112
+    Top = 232
+    Width = 73
+    Height = 21
+    TabOrder = 7
+    Text = '81'
+  end
+  object ClientSocket1: TClientSocket
+    Active = False
+    ClientType = ctNonBlocking
+    Port = 0
+    OnConnect = ClientSocket1Connect
+    Left = 152
+    Top = 8
+  end
+  object ServerSocket1: TServerSocket
+    Active = False
+    Port = 0
+    ServerType = stNonBlocking
+    OnClientRead = ServerSocket1ClientRead
+    Left = 200
+    Top = 8
+  end
+end
